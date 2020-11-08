abb1943a4d4f201f7b2f8b4b97165332f642d2f9
Genexys <magicilusha@gmail.com>
2020-11-08 12:06:05 +0300

    change routing and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-
-import { Router, Redirect, Route, Switch } from 'react-router-dom';
-
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -12,16 +9,14 @@ import Menu from './components/Menu';
 
 import HomePage from './pages/HomePage';
 import PokedexPage from './pages/PokedexPage';
-
-import s from './App.module.scss';
 import ErrorPage from './pages/ErrorPage';
 
-const customHistory = createBrowserHistory();
+import s from './App.module.scss';
 
 const App = () => {
   return (
     <div className={cn(s.app)}>
-      <Router history={customHistory}>
+      <Router>
         <Switch>
           <Route
             render={(props) => {
