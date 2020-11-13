5f26dc4ca0c3a093c99fd72504ade16b0a7516d1
Genexys <magicilusha@gmail.com>
2020-11-13 21:57:25 +0300

    add card and change routing

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import cn from 'classnames';
 import Heading from '../Heading';
 
 import s from './Card.module.scss';
 
-interface IPokemonsData {
-  data: {
-    ['name_clean']: string;
-    abilities: string[];
-    stats: {
-      [n: string]: number;
-    };
-    types: string[];
-    img: string;
-    name: string;
-    ['base_experience']: number;
-    height: number;
-    id: number;
-    ['is_default']: boolean;
-    order: number;
-    weight: number;
-  };
-}
+type BGTypes = {
+  [n: string]: string;
+};
 
-const Card: React.FC<IPokemonsData> = ({ data }) => {
+const BG: BGTypes = {
+  poison: '#a974bc',
+  psychic: '#a974bc',
+  fairy: '#a974bc',
+  ghost: '#a974bc',
+  grass: '#73d677',
+  bug: '#73d677',
+  ground: '#9b897b',
+  electric: '#f7c545',
+  gosth: '#76aadb',
+  normal: '#76aadb',
+  fire: '#f76545',
+  fighting: '#f76545',
+  dragon: '#f76545',
+  ice: '#a2cff0',
+  water: '#a2cff0',
+  stile: '#a1a1a1',
+  dark: '#a1a1a1',
+  rock: '#a1a1a1',
+  flying: '#A890F0',
+  steel: '#B7B7CE',
+};
+
+const Card = ({ data }) => {
+  const bgStyle: CSSProperties = {
+    background:
+      data.types.length > 1
+        ? `linear-gradient(270deg, ${BG[data.types[0]]} 0.15%, ${BG[data.types[1]]} 100%)`
+        : `background: ${BG[data.types[0]]}`,
+  };
+
   return (
     <div className={s.root}>
       <div className={s.infoWrap}>
@@ -50,7 +65,7 @@ const Card: React.FC<IPokemonsData> = ({ data }) => {
           ))}
         </div>
       </div>
-      <div className={cn(s.pictureWrap, s[data.types[0]])}>
+      <div className={cn(s.pictureWrap, s[data.types[0]])} style={bgStyle}>
         <img src={data.img} alt={data.name} />
       </div>
     </div>
---
+++ src/components/PokebolLoader/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import s from './PokebolLoader.module.scss';
+
+const PokebolLoader = () => <div className={s.pokeball} />;
+
+export default PokebolLoader;
